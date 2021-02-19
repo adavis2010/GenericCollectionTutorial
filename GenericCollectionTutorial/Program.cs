13504fea67152d7efed06d1dbe831e96d086eb11
max-student <max-student@maxtrain.com>
2021-02-19 10:49:57 -0500

    commit after initial commit

--- GenericCollectionTutorial/Program.cs
+++ GenericCollectionTutorial/Program.cs
@@ -4,6 +4,41 @@ using System.Collections.Generic;
 namespace GenericCollectionTutorial {
     class Program {
         static void Main(string[] args) {
+            // create list for weather class 
+            var WxHistory = new List<Weather>(3);
+            var DtThr = new Weather {
+                Today = new DateTime(2021, 2, 18),
+                Temperature = 19,
+                Percipitation = 3
+            };
+            WxHistory.Add(DtThr);
+            var DtWed = new Weather {
+                Today = new DateTime(2021, 2, 17),
+                Temperature = 4,
+                Percipitation = 0
+            };
+            WxHistory.Add(DtWed);
+            var DtTue = new Weather {
+                Today = new DateTime(2021, 2, 16),
+                Temperature = 25,
+                Percipitation = 2
+            };
+
+            WxHistory.Add(DtTue);
+            // foreach statement
+            foreach (var day in WxHistory) {
+                //concat statement with + sign so we have multiple lines so message isnt all on one line
+                var msg = $"Weather for {day.Today.ToString("MM dd,yyyy")}" +
+                    $" Percipitation was {day.Percipitation} inches" +
+                    $" Temperature was {day.Temperature} degrees";
+                    Console.WriteLine(msg);
+            }
+            
+           
+
+
+
+
             // created generic list
             var ints = new List<int>();
             // creating another list (strings)
@@ -12,8 +47,15 @@ namespace GenericCollectionTutorial {
             }; 
             //Print favorite color list
             Console.WriteLine($"Favorite color count is {strs.Count}");
+            //Sort Colors
+            strs.Sort();
+            //for each statement on sorts
+            foreach (var color in strs) {
+                //print sorted list
+            Console.WriteLine($"Color is {color}");
+            }
 
-            //add to our collection
+            //initial add to our collection
 
             ints.Add(7);
             ints.Add(10);
@@ -23,7 +65,7 @@ namespace GenericCollectionTutorial {
             ints.Add(22);
             // used for each statement 
             foreach (var i in ints) {
-                //print statement for favorite numbers
+              //print statement for favorite numbers
                 Console.WriteLine($"Favorite number {i}");
             }
 
---
+++ GenericCollectionTutorial/Weather.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericCollectionTutorial {
+    class Weather {
+
+        public DateTime Today { get; set; } = DateTime.Now;
+        public int Temperature { get; set; } 
+        public Double Percipitation { get; set; }
+
+
+
+    }
+
+
+}
